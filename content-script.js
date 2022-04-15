cd83547c9035af58fadf73c24690033007f1f32b
alarivan <alarivan@pm.me>
2022-04-15 15:37:34 -0700

    Fixformatting

--- content-script.js
+++ content-script.js
@@ -2,24 +2,28 @@ const targetNode = document.querySelector(".app-root");
 const observerOptions = {
   childList: true,
   attributes: false,
-  subtree: true
-}
+  subtree: true,
+};
 
 function callback(mutationList, observer) {
-  mutationList.forEach( (mutation) => {
-    if(mutation.type === 'childList') {
-      const node = mutation.addedNodes[0] || mutation.removedNodes[0];;
-      if(node && node.classList && node.classList.contains("composer")) {
-        const actionType = mutation.addedNodes.length ? "added" : "removed"; 
+  mutationList.forEach((mutation) => {
+    if (mutation.type === "childList") {
+      const node = mutation.addedNodes[0] || mutation.removedNodes[0];
+      if (node && node.classList && node.classList.contains("composer")) {
+        const actionType = mutation.addedNodes.length ? "added" : "removed";
         switch (actionType) {
-          case 'added': {
-            const contentNode = document.querySelector(".main-area--with-toolbar")  
+          case "added": {
+            const contentNode = document.querySelector(
+              ".main-area--with-toolbar"
+            );
             const newWidth = contentNode.offsetWidth - node.offsetWidth - 30;
             contentNode.style.width = `${newWidth}px`;
             break;
           }
-          case 'removed': {
-            const contentNode = document.querySelector(".main-area--with-toolbar")  
+          case "removed": {
+            const contentNode = document.querySelector(
+              ".main-area--with-toolbar"
+            );
             contentNode.style.width = "";
             break;
           }
@@ -29,7 +33,7 @@ function callback(mutationList, observer) {
   });
 }
 
-if(window.innerWidth >= 1440) {
+if (window.innerWidth >= 1440) {
   const observer = new MutationObserver(callback);
   observer.observe(targetNode, observerOptions);
 }
