3b29a15a22bd64cf8ce1ae5f64cba94c8b8c5e83
alarivan <alarivan@pm.me>
2022-04-15 19:19:46 -0700

    Stretch composer full height of the main area

--- content-script.js
+++ content-script.js
@@ -1,9 +1,21 @@
+const GAP = 15;
+
 const targetNode = document.querySelector(".app-root");
 const observerOptions = {
   childList: true,
   attributes: false,
   subtree: true,
 };
+
+function getToolbarHeight() {
+  const header = document.querySelector("header.header");
+  const navToolbar = document.querySelector("nav.toolbar");
+  if (header && navToolbar) {
+    return header.offsetHeight + navToolbar.offsetHeight;
+  }
+
+  return 0;
+}
 
 function callback(mutationList, observer) {
   mutationList.forEach((mutation) => {
@@ -18,6 +30,9 @@ function callback(mutationList, observer) {
             );
             const newWidth = contentNode.offsetWidth - node.offsetWidth - 30;
             contentNode.style.width = `${newWidth}px`;
+            const minHeight = window.innerHeight - getToolbarHeight() - GAP;
+            node.style["max-height"] = `${minHeight}px`;
+            node.style.height = `100%`;
             break;
           }
           case "removed": {
@@ -25,6 +40,8 @@ function callback(mutationList, observer) {
               ".main-area--with-toolbar"
             );
             contentNode.style.width = "";
+            node.style["min-height"] = "";
+            node.style.height = "";
             break;
           }
         }
